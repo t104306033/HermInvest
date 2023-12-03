068319087ce81a2f483d16ff866252c11cf6cee1
isaac.lu <isaac.lu0897@gmail.com>
2023-12-03 17:17:09 +0800

    feat(herminvestcli): implement deleteion of transations functionality

--- cmd/hermInvestCli/delete.go
+++ cmd/hermInvestCli/delete.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"database/sql"
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -11,6 +15,55 @@ var deleteCmd = &cobra.Command{
 	Short: "Delete stock",
 	Long:  `Delete stock from the inventory by providing the transaction ID`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Not implemented!")
+		if len(args) != 1 {
+			fmt.Println("Please provide the transaction ID to delete.")
+			return
+		}
+
+		id, err := strconv.Atoi(args[0])
+		if err != nil {
+			fmt.Println("Invalid ID provided. Please provide a valid ID.")
+			return
+		}
+
+		// sqlite3 connection with  foreign keys enabled
+		var dbPath = "./internal/app/database/dev-database.db?_foreign_keys=true"
+
+		db, err := sql.Open("sqlite3", dbPath)
+		if err != nil {
+			fmt.Println("Error connecting to the database:", err)
+			return
+		}
+		defer db.Close()
+
+		confirm := confirmDeletion()
+		if confirm {
+			err = deleteTransaction(db, id)
+			if err != nil {
+				fmt.Println("Error deleting transaction:", err)
+				return
+			}
+			fmt.Println("Transaction deleted successfully!")
+		} else {
+			fmt.Println("Deletion cancelled.")
+		}
 	},
+}
+
+func confirmDeletion() bool {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Are you sure you want to delete this transaction? (yes/no): ")
+	text, _ := reader.ReadString('\n')
+	text = trimNewline(text)
+
+	return text == "yes"
+}
+
+func trimNewline(s string) string {
+	return s[:len(s)-1]
+}
+
+func deleteTransaction(db *sql.DB, id int) error {
+	_, err := db.Exec("DELETE FROM tblTransaction WHERE id = ?", id)
+	return err
 }
