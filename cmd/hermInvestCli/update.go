9a8911b0330cb7fe2812f2aab55e48181c0ce20a
Isaac.lu <isaac.lu0897@gmail.com>
2023-12-18 21:49:21 +0800

    style(update.go): reorganize code

--- cmd/hermInvestCli/update.go
+++ cmd/hermInvestCli/update.go
@@ -21,55 +21,57 @@ var updateCmd = &cobra.Command{
 		"  - Update unit Price by ID:\n" +
 		"    hermInvestCli stock update 11 20.3",
 	Long: `Update the unit price of stock in the inventory using the transaction ID.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) < 2 {
-			fmt.Println("Please provide transaction ID and unit price")
-			cmd.Help()
-			return
-		}
-
-		transactionID, err := strconv.Atoi(args[0])
-		if err != nil {
-			fmt.Println("Error parsing integer: ", err)
-		}
-		unitPrice, err := strconv.ParseFloat(args[1], 64)
-		if err != nil {
-			fmt.Println("Error parsing float: ", err)
-		}
-
-		db, err := GetDBConnection()
-		if err != nil {
-			fmt.Println("Error geting DB connection: ", err)
-		}
-		defer db.Close()
-
-		// init transactionRepository
-		repo := &transactionRepository{db: db}
-
-		transactions, err := repo.queryTransactionByID(transactionID)
-		if err != nil {
-			fmt.Println("Error querying database:", err)
-		}
-
-		// TODO: check update work. ex: update a fake transaction ID to db
-		t := transactions[0]
-		t.unitPrice = unitPrice // update unit Price
-
-		// Recalculate
-		t.calculateTotalAmount()
-		t.calculateTaxesFromTotalAmount()
-
-		// update db
-		err = repo.updateTransaction(t)
-		if err != nil {
-			fmt.Println("Error updating stock information:", err)
-			return
-		}
-
-		fmt.Printf("Successfully updated transaction ID %d with new unit price %.2f\n", t.id, t.unitPrice)
-	},
+	Run:  updateRun,
 }
 
 func init() {
 	stockCmd.AddCommand(updateCmd)
+}
+
+func updateRun(cmd *cobra.Command, args []string) {
+	if len(args) < 2 {
+		fmt.Println("Please provide transaction ID and unit price")
+		cmd.Help()
+		return
+	}
+
+	transactionID, err := strconv.Atoi(args[0])
+	if err != nil {
+		fmt.Println("Error parsing integer: ", err)
+	}
+	unitPrice, err := strconv.ParseFloat(args[1], 64)
+	if err != nil {
+		fmt.Println("Error parsing float: ", err)
+	}
+
+	db, err := GetDBConnection()
+	if err != nil {
+		fmt.Println("Error geting DB connection: ", err)
+	}
+	defer db.Close()
+
+	// init transactionRepository
+	repo := &transactionRepository{db: db}
+
+	transactions, err := repo.queryTransactionByID(transactionID)
+	if err != nil {
+		fmt.Println("Error querying database:", err)
+	}
+
+	// TODO: check update work. ex: update a fake transaction ID to db
+	t := transactions[0]
+	t.unitPrice = unitPrice // update unit Price
+
+	// Recalculate
+	t.calculateTotalAmount()
+	t.calculateTaxesFromTotalAmount()
+
+	// update db
+	err = repo.updateTransaction(t)
+	if err != nil {
+		fmt.Println("Error updating stock information:", err)
+		return
+	}
+
+	fmt.Printf("Successfully updated transaction ID %d with new unit price %.2f\n", t.id, t.unitPrice)
 }
