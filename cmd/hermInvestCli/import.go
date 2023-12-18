a9772d996814fa397996bfd594e69b559c54db8a
Isaac.lu <isaac.lu0897@gmail.com>
2023-12-18 21:51:50 +0800

    style(import.go): reorganize code

--- cmd/hermInvestCli/import.go
+++ cmd/hermInvestCli/import.go
@@ -33,93 +33,7 @@ var importCmd = &cobra.Command{
 		"Import stock from csv file.\n" +
 		"Please check your csv file has column stockNo type quantity unitPrice.",
 	Args: cobra.MinimumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		filePath := args[0]
-		skipHeader, _ := cmd.Flags().GetBool("skipHeader")
-		indexes, _ := cmd.Flags().GetString("swapColumn")
-
-		// need testcase check file path exist
-		// need testcase check file permission
-		file, err := os.Open(filePath)
-		if err != nil {
-			fmt.Println("Error opening the file: ", err)
-			return
-		}
-		defer file.Close()
-
-		// need testcase check it is not a dir
-		fileInfo, _ := file.Stat()
-		if fileInfo.IsDir() {
-			fmt.Println("Error filePath is not a file.")
-			return
-		}
-
-		fileReader := csv.NewReader(file)
-
-		if skipHeader {
-			_, err := fileReader.Read()
-			if err != nil {
-				if err == io.EOF {
-					fmt.Println("Error file empty.")
-				} else {
-					fmt.Println("Error reading header: ", err)
-				}
-				return
-			}
-		}
-
-		rows, err := fileReader.ReadAll()
-		if err != nil {
-			fmt.Println("Error reading rows: ", err)
-			return
-		}
-
-		db, err := GetDBConnection()
-		if err != nil {
-			fmt.Println("Error geting DB connection: ", err)
-		}
-		defer db.Close()
-
-		// init transactionRepository
-		repo := &transactionRepository{db: db}
-
-		var transactions []*Transaction
-		for _, row := range rows {
-			if indexes != "" {
-				row, err = swapColumn(row, indexes)
-				if err != nil {
-					fmt.Println("Error swaping column:", err)
-					return
-				}
-			}
-
-			stockNo, tranType, quantity, unitPrice, date, err := ParseTransactionForAddCmd(row)
-			if err != nil {
-				fmt.Println("Error parsing transaction data:", err)
-				return
-			}
-
-			t := newTransactionFromInput(stockNo, date, quantity, tranType, unitPrice)
-			transactions = append(transactions, t)
-		}
-
-		// TODO: create Transactions, bulk insert? Finally, I choose begin a db transaction
-		ids, err := repo.createTransactions(transactions)
-		if err != nil {
-			fmt.Println("Error creating transaction: ", err)
-		}
-
-		fmt.Println("inserted ids:", ids)
-
-		// 	// Bool control show result or not
-		// 	transactions, err := repo.queryTransactionByID(id)
-		// 	if err != nil {
-		// 		fmt.Println("Error querying database:", err)
-		// 	}
-
-		// Print out result
-		// displayResults(transactions)
-	},
+	Run:  importRun,
 }
 
 func init() {
@@ -127,6 +41,94 @@ func init() {
 
 	importCmd.Flags().Bool("skipHeader", false, "Ignore header")
 	importCmd.Flags().String("swapColumn", "", "Swap column")
+}
+
+func importRun(cmd *cobra.Command, args []string) {
+	filePath := args[0]
+	skipHeader, _ := cmd.Flags().GetBool("skipHeader")
+	indexes, _ := cmd.Flags().GetString("swapColumn")
+
+	// need testcase check file path exist
+	// need testcase check file permission
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Error opening the file: ", err)
+		return
+	}
+	defer file.Close()
+
+	// need testcase check it is not a dir
+	fileInfo, _ := file.Stat()
+	if fileInfo.IsDir() {
+		fmt.Println("Error filePath is not a file.")
+		return
+	}
+
+	fileReader := csv.NewReader(file)
+
+	if skipHeader {
+		_, err := fileReader.Read()
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Error file empty.")
+			} else {
+				fmt.Println("Error reading header: ", err)
+			}
+			return
+		}
+	}
+
+	rows, err := fileReader.ReadAll()
+	if err != nil {
+		fmt.Println("Error reading rows: ", err)
+		return
+	}
+
+	db, err := GetDBConnection()
+	if err != nil {
+		fmt.Println("Error geting DB connection: ", err)
+	}
+	defer db.Close()
+
+	// init transactionRepository
+	repo := &transactionRepository{db: db}
+
+	var transactions []*Transaction
+	for _, row := range rows {
+		if indexes != "" {
+			row, err = swapColumn(row, indexes)
+			if err != nil {
+				fmt.Println("Error swaping column:", err)
+				return
+			}
+		}
+
+		stockNo, tranType, quantity, unitPrice, date, err := ParseTransactionForAddCmd(row)
+		if err != nil {
+			fmt.Println("Error parsing transaction data:", err)
+			return
+		}
+
+		t := newTransactionFromInput(stockNo, date, quantity, tranType, unitPrice)
+		transactions = append(transactions, t)
+	}
+
+	// TODO: create Transactions, bulk insert? Finally, I choose begin a db transaction
+	ids, err := repo.createTransactions(transactions)
+	if err != nil {
+		fmt.Println("Error creating transaction: ", err)
+	}
+
+	fmt.Println("inserted ids:", ids)
+
+	// 	// Bool control show result or not
+	// 	transactions, err := repo.queryTransactionByID(id)
+	// 	if err != nil {
+	// 		fmt.Println("Error querying database:", err)
+	// 	}
+
+	// Print out result
+	// displayResults(transactions)
 }
 
 func swapColumn(row []string, indexes string) ([]string, error) {
