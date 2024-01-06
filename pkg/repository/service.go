cf19b48718e5b938afe76928108413cceb335115
Isaac.lu <isaac.lu0897@gmail.com>
2024-01-06 23:20:05 +0800

    style(repo): refactor repo style and extract service to a file

---
+++ pkg/repository/service.go
@@ -0,0 +1,148 @@
+package repository
+
+import (
+	"HermInvest/pkg/model"
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+// Service Tier
+
+// addTransactionTailRecursion add new transaction records with tail recursion,
+// When adding, inventory and transaction history, especially write-offs and
+// tails, need to be considered.
+func (repo *TransactionRepositoryGorm) addTransactionTailRecursion(newTransaction *model.Transaction, remainingQuantity int) (*model.Transaction, error) {
+	// Principles:
+	// 1. Ensure that each transaction has a corresponding transaction record.
+	// 2. Update inventory quantities based on transactions, including adding,
+	//    reducing, or deleting inventory.
+	// 3. Depending on the transaction situation, only transaction history can
+	//    be added and cannot be modified or deleted.
+	// 4. For insufficient write-off quantities, recursive processing is used
+	//    to ensure that the write-off is completed.
+
+	// Cases:
+	// 1. Newly added: If there is no transaction in the inventory (A) or
+	//    the new transaction is the same as the oldest transaction in the
+	//    inventory (B), add it directly to the inventory.
+	// 2. Write-off:
+	// 	* Sufficient inventory: If the inventory quantity is sufficient,
+	//    update the inventory quantity (C) or delete the inventory (D), and
+	//    add the corresponding transaction history.
+	// 	* Insufficient inventory: If the inventory quantity can't be Write-off.
+	//    Recurse until success (E). The termination condition is A B C D.
+	//  * Over inventory: Write-off over than inventory (F).
+
+	// TODO: This func should be moved to service tier.
+
+	earliestTransaction, err := repo.FindEarliestTransactionByStockNo(newTransaction.StockNo)
+	if err != nil {
+		if err != gorm.ErrRecordNotFound {
+			return nil, fmt.Errorf("error finding first purchase: %v", err)
+		}
+		// Case A
+		earliestTransaction.TranType = newTransaction.TranType
+	}
+
+	if earliestTransaction.TranType == newTransaction.TranType {
+		if newTransaction.Quantity != remainingQuantity {
+			// Case F
+			newTransaction.SetQuantity(newTransaction.Quantity - remainingQuantity)
+			_, err = repo.CreateTransactionHistory(newTransaction)
+			if err != nil {
+				// handle create transaction history failed
+			}
+			newTransaction.SetQuantity(remainingQuantity)
+		}
+
+		// Case B
+		id, err := repo.CreateTransaction(newTransaction)
+		if err != nil {
+			return nil, fmt.Errorf("error creating transaction: %v", err)
+		}
+		transaction, err := repo.QueryTransactionByID(id)
+		if err != nil {
+			return nil, fmt.Errorf("error querying database: %v", err)
+		}
+
+		return transaction, nil
+	} else {
+		if earliestTransaction.Quantity > remainingQuantity {
+			// Case C
+
+			// Create a copy for adding stock history
+			stockHistoryAdd := &model.Transaction{}
+			*stockHistoryAdd = *earliestTransaction
+			// var stockHistoryAdd *model.Transaction // why can't use it, study it
+			// *stockHistoryAdd = *earliestTransaction
+
+			// add transaction history
+			stockHistoryAdd.SetQuantity(remainingQuantity)
+			_, err = repo.CreateTransactionHistory(stockHistoryAdd)
+			if err != nil {
+				// handle create transaction history failed
+			}
+			_, err = repo.CreateTransactionHistory(newTransaction)
+			if err != nil {
+				// handle create transaction history failed
+			}
+
+			// Update stock inventory
+			earliestTransaction.SetQuantity(earliestTransaction.Quantity - remainingQuantity)
+			err := repo.UpdateTransaction(earliestTransaction.ID, earliestTransaction)
+			if err != nil {
+				// handle update transaction failed
+			}
+
+			return earliestTransaction, nil
+		} else if earliestTransaction.Quantity == remainingQuantity {
+			// Case D
+
+			// add transaction history
+			_, err = repo.CreateTransactionHistory(earliestTransaction)
+			if err != nil {
+				// handle create transaction history failed
+			}
+			_, err = repo.CreateTransactionHistory(newTransaction)
+			if err != nil {
+				// handle create transaction history failed
+			}
+			// delete stock inventory
+			err = repo.DeleteTransaction(earliestTransaction.ID)
+			if err != nil {
+				// handle create transaction history failed
+			}
+
+			// Or use move
+
+			return nil, nil
+		} else { // earliestTransaction.Quantity < remainingQuantity
+			// Case E
+
+			// add transaction history
+			_, err = repo.CreateTransactionHistory(earliestTransaction)
+			if err != nil {
+				// handle create transaction history failed
+			}
+
+			// delete stock inventory
+			err = repo.DeleteTransaction(earliestTransaction.ID)
+			if err != nil {
+				// handle create transaction history failed
+			}
+
+			remainingQuantity = remainingQuantity - earliestTransaction.Quantity
+
+			return repo.addTransactionTailRecursion(newTransaction, remainingQuantity)
+		}
+	}
+}
+
+// AddTransaction add the transaction from the input to the inventory.
+// It will add or update transactions in the inventory and add history.
+// Return the modified transaction record in the inventory
+func (repo *TransactionRepositoryGorm) AddTransaction(newTransaction *model.Transaction) (*model.Transaction, error) {
+	remainingQuantity := newTransaction.Quantity
+	return repo.addTransactionTailRecursion(newTransaction, remainingQuantity)
+}
--- pkg/repository/transaction.go
+++ pkg/repository/transaction.go
@@ -15,8 +15,9 @@ func NewTransactionRepositoryGorm(db *gorm.DB) *TransactionRepositoryGorm {
 	return &TransactionRepositoryGorm{db: db}
 }
 
-// ---
-// Transaction
+/******************************************************************************
+ *                             Transaction Table                              *
+ ******************************************************************************/
 
 // CreateTransaction: insert transaction and return inserted id
 func (repo *TransactionRepositoryGorm) CreateTransaction(t *model.Transaction) (int, error) {
@@ -42,37 +43,6 @@ func (repo *TransactionRepositoryGorm) CreateTransactions(ts []*model.Transactio
 
 	return insertedIDs, nil
 }
-
-// ---
-// Transaction History
-
-// CreateTransactionHistory: insert transaction and return inserted id
-func (repo *TransactionRepositoryGorm) CreateTransactionHistory(t *model.Transaction) (int, error) {
-	result := repo.db.Debug().Table("tblTransactionHistory").Create(&t)
-	if result.Error != nil {
-		return 0, result.Error
-	}
-
-	return t.ID, nil
-}
-
-// testcase begin, commit, rollback
-// CreateTransactionHistorys: insert transactions and return inserted ids
-func (repo *TransactionRepositoryGorm) CreateTransactionHistorys(ts []*model.Transaction) ([]int, error) {
-	result := repo.db.Debug().Table("tblTransactionHistory").Create(&ts)
-	if result.Error != nil {
-		return nil, result.Error
-	}
-
-	var insertedIDs []int
-	for _, t := range ts {
-		insertedIDs = append(insertedIDs, t.ID)
-	}
-
-	return insertedIDs, nil
-}
-
-// ---
 
 func (repo *TransactionRepositoryGorm) FindEarliestTransactionByStockNo(stockNo string) (*model.Transaction, error) {
 	var transaction model.Transaction
@@ -135,6 +105,15 @@ func (repo *TransactionRepositoryGorm) UpdateTransaction(id int, t *model.Transa
 	return nil
 }
 
+// deleteAlltblTransaction
+func (repo *TransactionRepositoryGorm) DeleteAlltblTransaction() error {
+	if err := repo.db.Debug().Exec("DELETE FROM tblTransaction").Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (repo *TransactionRepositoryGorm) DeleteTransaction(id int) error {
 	result := repo.db.Table("tblTransaction").Delete(&model.Transaction{ID: id})
 	return result.Error
@@ -144,6 +123,48 @@ func (repo *TransactionRepositoryGorm) DeleteTransactions(ids []int) error {
 	result := repo.db.Table("tblTransaction").Delete(&model.Transaction{}, "id IN ?", ids)
 	return result.Error
 }
+
+/******************************************************************************
+ *                         Transaction History Table                          *
+ ******************************************************************************/
+
+// CreateTransactionHistory: insert transaction and return inserted id
+func (repo *TransactionRepositoryGorm) CreateTransactionHistory(t *model.Transaction) (int, error) {
+	result := repo.db.Debug().Table("tblTransactionHistory").Create(&t)
+	if result.Error != nil {
+		return 0, result.Error
+	}
+
+	return t.ID, nil
+}
+
+// CreateTransactionHistorys: insert transactions and return inserted ids
+func (repo *TransactionRepositoryGorm) CreateTransactionHistorys(ts []*model.Transaction) ([]int, error) {
+	result := repo.db.Debug().Table("tblTransactionHistory").Create(&ts)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	var insertedIDs []int
+	for _, t := range ts {
+		insertedIDs = append(insertedIDs, t.ID)
+	}
+
+	return insertedIDs, nil
+}
+
+// deleteAlltblTransactionHistory
+func (repo *TransactionRepositoryGorm) DeleteAlltblTransactionHistory() error {
+	if err := repo.db.Debug().Exec("DELETE FROM tblTransactionHistory").Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+/******************************************************************************
+ *                          Capital Reduction Table                           *
+ ******************************************************************************/
 
 // QueryCapitalReductionAll
 func (repo *TransactionRepositoryGorm) QueryCapitalReductionAll() ([]*model.CapitalReduction, error) {
@@ -158,6 +179,19 @@ func (repo *TransactionRepositoryGorm) QueryCapitalReductionAll() ([]*model.Capi
 	// }
 
 	return capitalReductions, nil
+}
+
+/******************************************************************************
+ *                          Transaction Record Table                          *
+ ******************************************************************************/
+
+// insertTransactionRecordSys
+func (repo *TransactionRepositoryGorm) InsertTransactionRecordSys(tr *model.TransactionRecord) error {
+	if err := repo.db.Debug().Table("tblTransactionRecordSys").Create(tr).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // QueryTransactionRecordByStockNo
@@ -175,22 +209,22 @@ func (repo *TransactionRepositoryGorm) QueryTransactionRecordByStockNo(stockNo s
 	return transactionRecords, nil
 }
 
-// deleteAlltblTransaction
-func (repo *TransactionRepositoryGorm) DeleteAlltblTransaction() error {
-	if err := repo.db.Debug().Exec("DELETE FROM tblTransaction").Error; err != nil {
-		return err
+// QueryTransactionRecordUnion
+func (repo *TransactionRepositoryGorm) QueryTransactionRecordUnion() ([]*model.TransactionRecord, error) {
+	// SQLite seems to help you sort items by primary key when you query via UNION keyword.
+	// Or you can add ORDER keyword in the last line to sort it.
+	var transactionRecords []*model.TransactionRecord
+	err := repo.db.Debug().Raw(`
+	SELECT date, time, stockNo, tranType, quantity, unitPrice
+	FROM tblTransactionRecord
+	UNION SELECT * FROM tblTransactionRecordSys
+	`).Scan(&transactionRecords).Error
+
+	if err != nil {
+		return nil, nil
 	}
 
-	return nil
-}
-
-// deleteAlltblTransactionHistory
-func (repo *TransactionRepositoryGorm) DeleteAlltblTransactionHistory() error {
-	if err := repo.db.Debug().Exec("DELETE FROM tblTransactionHistory").Error; err != nil {
-		return err
-	}
-
-	return nil
+	return transactionRecords, nil
 }
 
 // deleteAllTransactionRecordSys
@@ -201,6 +235,10 @@ func (repo *TransactionRepositoryGorm) DeleteAllTransactionRecordSys() error {
 
 	return nil
 }
+
+/******************************************************************************
+ *                                    Note                                    *
+ ******************************************************************************/
 
 // QueryUnionNote
 func (repo *TransactionRepositoryGorm) QueryUnionNote() {
@@ -253,171 +291,4 @@ func (repo *TransactionRepositoryGorm) QueryUnionNote() {
 	})
 
 	fmt.Println(sql)
-}
-
-// QueryTransactionRecordUnion
-func (repo *TransactionRepositoryGorm) QueryTransactionRecordUnion() ([]*model.TransactionRecord, error) {
-	// SQLite seems to help you sort items by primary key when you query via UNION keyword.
-	// Or you can add ORDER keyword in the last line to sort it.
-	var transactionRecords []*model.TransactionRecord
-	err := repo.db.Debug().Raw(`
-	SELECT date, time, stockNo, tranType, quantity, unitPrice
-	FROM tblTransactionRecord
-	UNION SELECT * FROM tblTransactionRecordSys
-	`).Scan(&transactionRecords).Error
-
-	if err != nil {
-		return nil, nil
-	}
-
-	return transactionRecords, nil
-}
-
-// insertTransactionRecordSys
-func (repo *TransactionRepositoryGorm) InsertTransactionRecordSys(tr *model.TransactionRecord) error {
-	if err := repo.db.Debug().Table("tblTransactionRecordSys").Create(tr).Error; err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// Service Tier
-
-// addTransactionTailRecursion add new transaction records with tail recursion,
-// When adding, inventory and transaction history, especially write-offs and
-// tails, need to be considered.
-func (repo *TransactionRepositoryGorm) addTransactionTailRecursion(newTransaction *model.Transaction, remainingQuantity int) (*model.Transaction, error) {
-	// Principles:
-	// 1. Ensure that each transaction has a corresponding transaction record.
-	// 2. Update inventory quantities based on transactions, including adding,
-	//    reducing, or deleting inventory.
-	// 3. Depending on the transaction situation, only transaction history can
-	//    be added and cannot be modified or deleted.
-	// 4. For insufficient write-off quantities, recursive processing is used
-	//    to ensure that the write-off is completed.
-
-	// Cases:
-	// 1. Newly added: If there is no transaction in the inventory (A) or
-	//    the new transaction is the same as the oldest transaction in the
-	//    inventory (B), add it directly to the inventory.
-	// 2. Write-off:
-	// 	* Sufficient inventory: If the inventory quantity is sufficient,
-	//    update the inventory quantity (C) or delete the inventory (D), and
-	//    add the corresponding transaction history.
-	// 	* Insufficient inventory: If the inventory quantity can't be Write-off.
-	//    Recurse until success (E). The termination condition is A B C D.
-	//  * Over inventory: Write-off over than inventory (F).
-
-	// TODO: This func should be moved to service tier.
-
-	earliestTransaction, err := repo.FindEarliestTransactionByStockNo(newTransaction.StockNo)
-	if err != nil {
-		if err != gorm.ErrRecordNotFound {
-			return nil, fmt.Errorf("error finding first purchase: %v", err)
-		}
-		// Case A
-		earliestTransaction.TranType = newTransaction.TranType
-	}
-
-	if earliestTransaction.TranType == newTransaction.TranType {
-		if newTransaction.Quantity != remainingQuantity {
-			// Case F
-			newTransaction.SetQuantity(newTransaction.Quantity - remainingQuantity)
-			_, err = repo.CreateTransactionHistory(newTransaction)
-			if err != nil {
-				// handle create transaction history failed
-			}
-			newTransaction.SetQuantity(remainingQuantity)
-		}
-
-		// Case B
-		id, err := repo.CreateTransaction(newTransaction)
-		if err != nil {
-			return nil, fmt.Errorf("error creating transaction: %v", err)
-		}
-		transaction, err := repo.QueryTransactionByID(id)
-		if err != nil {
-			return nil, fmt.Errorf("error querying database: %v", err)
-		}
-
-		return transaction, nil
-	} else {
-		if earliestTransaction.Quantity > remainingQuantity {
-			// Case C
-
-			// Create a copy for adding stock history
-			stockHistoryAdd := &model.Transaction{}
-			*stockHistoryAdd = *earliestTransaction
-			// var stockHistoryAdd *model.Transaction // why can't use it, study it
-			// *stockHistoryAdd = *earliestTransaction
-
-			// add transaction history
-			stockHistoryAdd.SetQuantity(remainingQuantity)
-			_, err = repo.CreateTransactionHistory(stockHistoryAdd)
-			if err != nil {
-				// handle create transaction history failed
-			}
-			_, err = repo.CreateTransactionHistory(newTransaction)
-			if err != nil {
-				// handle create transaction history failed
-			}
-
-			// Update stock inventory
-			earliestTransaction.SetQuantity(earliestTransaction.Quantity - remainingQuantity)
-			err := repo.UpdateTransaction(earliestTransaction.ID, earliestTransaction)
-			if err != nil {
-				// handle update transaction failed
-			}
-
-			return earliestTransaction, nil
-		} else if earliestTransaction.Quantity == remainingQuantity {
-			// Case D
-
-			// add transaction history
-			_, err = repo.CreateTransactionHistory(earliestTransaction)
-			if err != nil {
-				// handle create transaction history failed
-			}
-			_, err = repo.CreateTransactionHistory(newTransaction)
-			if err != nil {
-				// handle create transaction history failed
-			}
-			// delete stock inventory
-			err = repo.DeleteTransaction(earliestTransaction.ID)
-			if err != nil {
-				// handle create transaction history failed
-			}
-
-			// Or use move
-
-			return nil, nil
-		} else { // earliestTransaction.Quantity < remainingQuantity
-			// Case E
-
-			// add transaction history
-			_, err = repo.CreateTransactionHistory(earliestTransaction)
-			if err != nil {
-				// handle create transaction history failed
-			}
-
-			// delete stock inventory
-			err = repo.DeleteTransaction(earliestTransaction.ID)
-			if err != nil {
-				// handle create transaction history failed
-			}
-
-			remainingQuantity = remainingQuantity - earliestTransaction.Quantity
-
-			return repo.addTransactionTailRecursion(newTransaction, remainingQuantity)
-		}
-	}
-}
-
-// AddTransaction add the transaction from the input to the inventory.
-// It will add or update transactions in the inventory and add history.
-// Return the modified transaction record in the inventory
-func (repo *TransactionRepositoryGorm) AddTransaction(newTransaction *model.Transaction) (*model.Transaction, error) {
-	remainingQuantity := newTransaction.Quantity
-	return repo.addTransactionTailRecursion(newTransaction, remainingQuantity)
 }
