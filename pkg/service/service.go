8c09948ecd3487c6421e79accc0452877aafcb1e
Isaac.lu <isaac.lu0897@gmail.com>
2024-04-28 16:47:44 +0800

    refactor(serv): extract RebuildCapitalReduction

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -15,39 +15,6 @@ func NewService(repository model.Repositorier) *service {
 	return &service{repo: repository}
 }
 
-func (serv *service) Test() {
-
-	tx := serv.repo.Begin()
-	txRepo := serv.repo.WithTrx(tx)
-
-	tr1 := model.NewTransactionRecord("2022-10-11", "08:02:00", "2330", 1, 3000, 25.4)
-	txRepo.InsertTransactionRecordSys(tr1)
-	// txRepo.Commit()
-	tr2 := model.NewTransactionRecord("2022-10-11", "08:03:00", "2330", 1, 3000, 25.4)
-	txRepo.InsertTransactionRecordSys(tr2)
-	txRepo.Rollback()
-	// txRepo.Tx.Commit()
-
-}
-
-func (serv *service) Test2() {
-
-	tr1 := model.NewTransactionRecord("2022-10-11", "08:04:00", "2330", 1, 3000, 25.4)
-	serv.repo.InsertTransactionRecordSys(tr1)
-	// serv.repo.DB.Commit()
-	tr2 := model.NewTransactionRecord("2022-10-11", "08:05:00", "2330", 1, 3000, 25.4)
-	serv.repo.InsertTransactionRecordSys(tr2)
-
-	// serv.repo.DB.Rollback()
-
-	// serv.repo.DB.Rollback()
-
-	// serv.repo.WithTrx(tx).DB.Rollback()
-
-	// tx.DB.Commit()
-
-}
-
 // addTransactionTailRecursion add new transaction records with tail recursion,
 // When adding, inventory and transaction history, especially write-offs and
 // tails, need to be considered.
@@ -237,4 +204,64 @@ func (serv *service) QueryTransactionRecordUnion() ([]*model.TransactionRecord, 
 }
 func (serv *service) CreateTransactions(ts []*model.Transaction) ([]int, error) {
 	return serv.repo.CreateTransactions(ts)
+}
+
+// ---
+
+func (serv *service) RebuildCapitalReduction() {
+
+	serv.DeleteAllTransactionRecordSys()
+
+	// 1. Query all transaction records from tblCapitalReduction
+	crs, _ := serv.QueryCapitalReductionAll()
+
+	// 2. Iterate over each capital reduction record
+	for _, cr := range crs {
+		fmt.Println("\n---\n", cr)
+		// Query transaction records by stock number
+		trs, _ := serv.QueryTransactionRecordByStockNo(cr.StockNo, cr.CapitalReductionDate)
+
+		remainingTrs := make([]*model.TransactionRecord, 0)
+		for _, tr := range trs {
+			if tr.TranType > 0 {
+				remainingTrs = append(remainingTrs, tr)
+			} else {
+				qty := tr.Quantity
+				for qty > 0 && len(remainingTrs) > 0 {
+					var remove *model.TransactionRecord
+					remove, remainingTrs = remainingTrs[0], remainingTrs[1:]
+					qty -= remove.Quantity
+				}
+			}
+		}
+		var totalQuantity int
+		var totalAmount int
+		for _, tr := range remainingTrs {
+			fmt.Println(tr)
+			totalQuantity += tr.TranType * tr.Quantity
+			totalAmount += int(float64(tr.Quantity) * tr.UnitPrice)
+		}
+		fmt.Println(totalQuantity, totalAmount)
+		var avgUnitPrice float64 = float64(totalAmount) / float64(totalQuantity)
+		fmt.Printf("%.2f\n", avgUnitPrice)
+
+		// 3. insert into tblTransactionRecordSys
+		capitalReductionRecord := model.NewTransactionRecord(
+			cr.CapitalReductionDate, "08:00:00", cr.StockNo, -1, totalQuantity, avgUnitPrice)
+		serv.InsertTransactionRecordSys(capitalReductionRecord)
+
+		newStockNo := cr.NewStockNo
+		if newStockNo == "" {
+			newStockNo = cr.StockNo
+		}
+
+		newQuantity := int(float64(totalQuantity) * (1 - cr.Ratio))
+		newAvgUnitPrice := (avgUnitPrice - cr.Cash) / (1 - cr.Ratio)
+		newCapitalReductionRecord := model.NewTransactionRecord(
+			cr.DistributionDate, "08:00:10", newStockNo, 1, newQuantity, newAvgUnitPrice)
+		serv.InsertTransactionRecordSys(newCapitalReductionRecord)
+
+		// deal with cash from tblCapitalReduction
+	}
+
 }
